ef952e1fc25ae709fa37721086c86fadb43066f1
lucastercas <lucasmtercas@gmail.com>
2019-08-07 22:58:36 +0000

    Fix errors on install script

--- install.rb
+++ install.rb
@@ -25,21 +25,21 @@ end
 
 def createPartition(disk, partition)
     puts "=== Creating partition #{partition[0]} - Size: #{partition[2]} - #{partition[3]} ==="
-    puts "sgdisk -n #{partition[0]}:#{partition[2]}:#{partition[3]} -c #{partition[0]}:\"#{partition[1]}\" -t #{partition[0]}:#{partition[4]} #{disk}"
+    system "sgdisk -n #{partition[0]}:#{partition[2]}:#{partition[3]} -c #{partition[0]}:\"#{partition[1]}\" -t #{partition[0]}:#{partition[4]} #{disk}"
 end
 
 def formatPartition(disk, partition)
   volume = "#{disk}#{partition[0]}"
   case partition[4]
   when "ef00"
-    puts "mkfs.vfat -F32 #{volume}"
+    system "mkfs.vfat -F32 #{volume}"
   when "8200"
-    puts "mkswap #{volume}"
-    puts "swapon #{volume}"
+    system "mkswap #{volume}"
+    system "swapon #{volume}"
   when "8300"
-    puts "mkfs.ext4 #{volume}"
+    system "mkfs.ext4 #{volume}"
   when "8302"
-    puts "mkfs.ext4 #{volume}"
+    system "mkfs.ext4 #{volume}"
   else
     puts "Code #{partition[4]} not recognized"
   end
@@ -47,17 +47,18 @@ end
 
 def mountPartition(disk, partition)
   puts "=== Mounting Partiton #{partition[1]}"
-  puts "mkdir -p /mnt#{partition[5]}"
-  puts "mount #{disk}#{partition[0]} /mnt#{partition[5]}"
+  system "sudo mkdir -p /mnt#{partition[5]}"
+  system "sudo mount #{disk}#{partition[0]} /mnt#{partition[5]}"
 end
 
 def configureDisk(disk)
   puts "Clearing disk #{disk}"
-  puts "sgdisk -oZg #{disk}"
+  system "sgdisk -oZg #{disk}"
 
   partitions = CSV.read('partitions.csv')
 
-  partitions.each do |partition|
+  partitions.each.with_index do |partition, index|
+    if index == 0 then next end
     printPartition(partition)
     createPartition(disk, partition)
     formatPartition(disk, partition)
@@ -84,65 +85,63 @@ puts "##### Arch Install #####"
 
 puts "\n### Disk Preparation ###"
 
-puts  "== Configuring Time and Date =="
-puts "timedatectl set-ntp true"
+#puts  "== Configuring Time and Date =="
+#puts "timedatectl set-ntp true"
 
-puts "== Configuring Keyboard =="
-puts "=> Layout of the keyboard: (ex: br-abnt2)"
+#puts "== Configuring Keyboard =="
+#puts "=> Layout of the keyboard: (ex: br-abnt2)"
 #kb_layout = gets.chomp
-kb_layout = 'br-abnt2'
-puts "Configuring Keyboard to #{kb_layout}"
-puts "loadkeys #{kb_layout}"
+#kb_layout = 'br-abnt2'
+#puts "Configuring Keyboard to #{kb_layout}"
+#puts "loadkeys #{kb_layout}"
 
 puts  "== Configuring Disk =="
 puts "=> Disk To Configure: (Ex: /dev/sda)"
-#disk = gets.chomp
-disk = '/dev/sda'
+disk = gets.chomp
 configureDisk(disk)
 
-puts  "== Configuring Pacstrap =="
-puts "pacstrap -i /mnt base-devel"
-puts "genfstab -U -p /mnt >> /mnt/etc/fstab"
-
-puts "\n### chroot ###"
-chroot_cmd = "arch-chroot /mnt"
-
-puts "=== Configuring Locale ==="
-puts "#{chroot_cmd} ln -sf /usr/share/zoneinfo/America/Fortaleza /etc/localtime"
-puts "#{chroot_cmd} sed -i 's/pt_BR.UTF-8/pt_BR.UTF-8/' /etc/locale.gen"
-puts "#{chroot_cmd} locale-gen"
-puts "#{chroot_cmd} hwclock --systohc"
-puts "#{chroot_cmd} echo 'LANG=pt_BR.UTF-8' >> /etc/locale.conf"
-
-puts "=== Installing Additional Packages ==="
-default_packages = getPackagesFromFile('default-packages.txt')
-puts "#{chroot_cmd} pacman -S #{default_packages}"
-
-puts "=== Configuring Hostname ==="
-puts "Hostname: "
+#puts  "== Configuring Pacstrap =="
+#system "pacstrap -i /mnt base-devel"
+#system "genfstab -U -p /mnt >> /mnt/etc/fstab"
+#
+#puts "\n### chroot ###"
+#chroot_cmd = "arch-chroot /mnt"
+#
+#puts "=== Configuring Locale ==="
+#system "#{chroot_cmd} ln -sf /usr/share/zoneinfo/America/Fortaleza /etc/localtime"
+#system "#{chroot_cmd} sed -i 's/pt_BR.UTF-8/pt_BR.UTF-8/' /etc/locale.gen"
+#system "#{chroot_cmd} locale-gen"
+#system "#{chroot_cmd} hwclock --systohc"
+#system "#{chroot_cmd} echo 'LANG=pt_BR.UTF-8' >> /etc/locale.conf"
+#
+#puts "=== Installing Additional Packages ==="
+#default_packages = getPackagesFromFile('default-packages.txt')
+#system "#{chroot_cmd} pacman -S #{default_packages}"
+#
+#puts "=== Configuring Hostname ==="
+#puts "Hostname: "
 #hostname = gets.chomp
-hostname = "hyperion"
-puts "#{chroot_cmd} echo #{hostname} >> /etc/hostname"
-
-puts "#{chroot_cmd} mkinitcpio -p linux"
-
-puts "#{chroot_cmd} passwd"
-
-puts "=== Configuring BootLoader (rEFInd) ==="
-puts "#{chroot_cmd} refind-install"
-
-puts "=== Configuring User ==="
-puts "User: "
-username = gets.chomp
-puts "#{chroot_cmd} useradd -m -G wheel -s /bin/zsh -c 'Lucas Tercas' #{username}"
-puts "#{chroot_cmd} passwd #{username}"
-
-puts "=== Enabling Services ==="
-puts "systemctl enable NetworkManager.service"
-puts "systemctl enable redshift-gtk.service"
-puts "systemctl enable bluetooth.service"
-puts "systemctl enable lightdm.service"
-puts "systemctl enable ntpd.service"
-puts "systemctl enable nptdate.service"
-puts "systemctl enable paccache.timer"
-puts "systemctl enable lightdm.service"
+#system "#{chroot_cmd} echo #{hostname} >> /etc/hostname"
+#
+#system "#{chroot_cmd} mkinitcpio -p linux"
+#
+#system "#{chroot_cmd} passwd"
+#
+#puts "=== Configuring BootLoader (rEFInd) ==="
+#system "#{chroot_cmd} refind-install"
+#
+#puts "=== Configuring User ==="
+#puts "User: "
+#username = gets.chomp
+#system "#{chroot_cmd} useradd -m -G wheel -s /bin/zsh -c 'Lucas Tercas' #{username}"
+#system "#{chroot_cmd} passwd #{username}"
+#
+#puts "=== Enabling Services ==="
+#system "#{chroot_cmd} systemctl enable NetworkManager.service"
+#system "#{chroot_cmd} systemctl enable redshift-gtk.service"
+#system "#{chroot_cmd} systemctl enable bluetooth.service"
+#system "#{chroot_cmd} systemctl enable lightdm.service"
+#system "#{chroot_cmd} systemctl enable ntpd.service"
+#system "#{chroot_cmd} systemctl enable nptdate.service"
+#system "#{chroot_cmd} systemctl enable paccache.timer"
+#system "#{chroot_cmd} systemctl enable lightdm.service"
