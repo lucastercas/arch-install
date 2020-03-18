73adf5d8ef575782c719ac7f699ef0cf90bd9364
lucastercas <lucasmtercas@gmail.com>
2020-03-18 01:32:26 -0200

    feat(install): Add dotfiles setup

--- aur.sh
+++ aur.sh
@@ -11,3 +11,22 @@ while IFS= read -r line; do
 done < "$aur_pkgs_file"
 cmd="yay -S $packages"
 echo "==> $cmd"; eval "$cmd"
+
+
+# Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Spaceship Prompt
+ZSH_CUSTOM="$HOME/.oh-my-zsh"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Node Manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# Dotfiles
+rm "$HOME/.zshrc"
+rm "$HOME/.bashrc"
+git clone --bare https://github.com/lucastercas/dotfiles "$HOME/.cfg"
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config checkout
