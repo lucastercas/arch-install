00d49d9020364cfd39dbb70adcb2e5cb6cf18e2a
lucastercas <lucasmtercas@gmail.com>
2020-07-12 00:17:28 -0300

    Try chroot runuser with -c

--- install.sh
+++ install.sh
@@ -108,11 +108,11 @@ cp -f ./files/70-synaptics.conf /mnt/etc/X11/xorg.conf.d/
 cp -f ./files/hosts /mnt/etc/
 cp -f ./files/lightdm.conf /mnt/etc/lightdm/
 
-cmd_as_user="$chroot_cmd runuser -l $username"
+cmd_as_user="${chroot_cmd} runuser -l ${username}"
 
 echo "#--- Install YAY ---#"
 yay_url="https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz"
-${cmd_as_user} curl "$yay_url" | tar xzv; cd yay && makepkg -si
+${cmd_as_user} -c "curl "$yay_url" | tar xzv; cd yay && makepkg -si"
 
 echo "#--- Install AUR Packages ---"
 aur_pkgs_file="./packages/aur.txt"
@@ -120,26 +120,26 @@ aur_packages=""
 while IFS= read -r line; do
   aur_packages="${packages} ${line}"
 done < "$aur_pkgs_file"
-${cmd_as_user} yay --noconfirm -S ${aur_packages}
+${cmd_as_user} -c "yay --noconfirm -S ${aur_packages}"
 
 echo "#--- Install NVM ---#"
 nvm_url="https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh"
-${cmd_as_user} curl -o- "$nvm_url" | bash
+${cmd_as_user} curl -o- "${nvm_url}" | bash
 
 echo "#--- Oh My ZSH ---#"
 omz_url="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
-${cmd_as_user} sh -c "$(curl -fsSL $omz_url)"
+${cmd_as_user} sh -c "$(curl -fsSL ${omz_url})"
 
 echo "#--- SpaceShip Prompt ---#"
 spaceship_url="https://github.com/denysdovhan/spaceship-prompt.git"
 zsh_dir="/home/$username/.oh-my-zsh"
-${cmd_as_user} git clone "$spaceship_url" "$zsh_dir/themes/spaceship-prompt"
-${cmd_as_user} ln -s "$zsh_dir/spaceship-prompt/spaceship.zsh-theme" "$zsh_dir/themes/spaceship.zsh-theme"
+${cmd_as_user} git clone "${spaceship_url}" "${zsh_dir}/themes/spaceship-prompt"
+${cmd_as_user} ln -s "${zsh_dir}/spaceship-prompt/spaceship.zsh-theme" "${zsh_dir}/themes/spaceship.zsh-theme"
 
 echo "#--- Dotfiles ---#"
 dotfiles_url="https://github.com/lucastercas/dotfiles"
-${cmd_as_user} git clone --base "$dotfiles_url" "/home/$username/.cfg"
-${cmd_as_user} git --git-dir="/home/$username/.cfg/" --work-tree="/home/$username" checkout
+${cmd_as_user} git clone --base "${dotfiles_url}" "/home/${username}/.cfg"
+${cmd_as_user} git --git-dir="/home/${username}/.cfg/" --work-tree="/home/${username}" checkout
 
 # echo "#--- Config files ---#"
 # ${chroot_cmd} runuser -l "$username" mkdir -p workspace
