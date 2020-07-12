43112d0d242e339698eb4ef207a2c69df665886a
lucastercas <lucasmtercas@gmail.com>
2020-07-12 01:59:45 -0300

    Put setup of configs on different files

--- install.sh
+++ install.sh
@@ -108,42 +108,5 @@ cp -f ./files/70-synaptics.conf /mnt/etc/X11/xorg.conf.d/
 cp -f ./files/hosts /mnt/etc/
 cp -f ./files/lightdm.conf /mnt/etc/lightdm/
 
-cmd_as_user="${chroot_cmd} runuser -l ${username}"
-
-echo "#--- Install YAY ---#"
-yay_url="https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz"
-${cmd_as_user} -c "curl ${yay_url} | tar xzv; cd yay && makepkg -si"
-
-echo "#--- Install AUR Packages ---"
-aur_pkgs_file="./packages/aur.txt"
-aur_packages=""
-while IFS= read -r line; do
-  aur_packages="${packages} ${line}"
-done < "$aur_pkgs_file"
-${cmd_as_user} -c "yay --noconfirm -S ${aur_packages}"
-
-echo "#--- Install NVM ---#"
-nvm_url="https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh"
-${cmd_as_user} -c "curl -o- ${nvm_url} | bash"
-
-echo "#--- Oh My ZSH ---#"
-omz_url="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
-${cmd_as_user} -c "sh -c $(curl -fsSL ${omz_url})"
-
-echo "#--- SpaceShip Prompt ---#"
-spaceship_url="https://github.com/denysdovhan/spaceship-prompt.git"
-zsh_dir="/home/$username/.oh-my-zsh"
-${cmd_as_user} -c "git clone ${spaceship_url} ${zsh_dir}/themes/spaceship-prompt"
-${cmd_as_user} -c "ln -s ${zsh_dir}/spaceship-prompt/spaceship.zsh-theme ${zsh_dir}/themes/spaceship.zsh-theme"
-
-echo "#--- Dotfiles ---#"
-dotfiles_url="https://github.com/lucastercas/dotfiles"
-${cmd_as_user} git clone --base "${dotfiles_url}" "/home/${username}/.cfg"
-${cmd_as_user} git --git-dir="/home/${username}/.cfg/" --work-tree="/home/${username}" checkout
-
-# echo "#--- Config files ---#"
-# ${chroot_cmd} runuser -l "$username" mkdir -p workspace
-# ${chroot_cmd} runuser -l "$username" git clone https://github.com/lucastercas/arch-install workspace/arch-install
-
-# echo "#--- Misc programs ---#"
-# ${chroot_cmd} runuser -l "$username" "$HOME/workspace/arch-install/misc.sh"
+cp setup.sh /mnt/home/lucastercas
+cp packages/aur.txt /mnt/home/lucastercas/aur.txt
