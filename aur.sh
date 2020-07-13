019ccd5805d2c40ff31abd4aa5970bb53f834e55
lucastercas <lucasmtercas@gmail.com>
2020-07-13 17:44:59 -0300

    Add packages needed for lightdm debug
    1. xorg-server-xephyr
    1. plymouth

--- aur.sh
+++ aur.sh
@@ -2,14 +2,14 @@
 
 set -eu
 
-echo "#--- Install YAY ---#"
-yay_url="https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz"
-curl ${yay_url} | tar -xzv; cd yay && makepkg -si
+#echo "#--- Install YAY ---#"
+#yay_url="https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz"
+#curl ${yay_url} | tar -xzv; cd yay && makepkg -si
 
 echo "#--- Install AUR Packages ---"
 aur_pkgs_file="./packages/aur.txt"
-aur_packages=""
+packages=""
 while IFS= read -r line; do
-  aur_packages="${packages} ${line}"
+  packages="${packages} ${line}"
 done < "$aur_pkgs_file"
-yay --noconfirm -S ${aur_packages}
+yay --noconfirm -S "${packages}"
