67280c7d7b847323a207007928c134ac2b27fab8
lucastercas <lucasmtercas@gmail.com>
2020-11-03 18:22:55 -0300

    fix(disks): Fix create_partitions disk

--- src/live/disks.rb
+++ src/live/disks.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/ruby
 
-def create_partitions(disk_name, partitions)
-  partitions.each do |partition|
+def create_partitions(disk_name, disk)
+  disk['partitions'].each do |partition|
     number = partition['number']
     system("sgdisk -n #{number}:0:+#{partition['size']} #{number}:#{partition['name']} -t #{number}:#{partition['partition_type']} /dev/#{disk_name}#{number}")
   end
 end
 
-def format_partitions(disk_name, partitions)
-  partitions["partitions"].each do |partition|
+def format_partitions(disk_name, disk)
+  disk["partitions"].each do |partition|
     puts("--> Formating partition #{partition['number']}")
     case partition["fs_type"]
     when "ext4"
@@ -19,8 +19,8 @@ def format_partitions(disk_name, partitions)
   end
 end
 
-def mount_partitions(disk_name, partitions)
-  partitions["partitions"].each do |partition|
+def mount_partitions(disk_name, disk)
+  disk["partitions"].each do |partition|
     puts("--> Mounting partition #{partition['number']}")
     system("mkdir -p /mnt/#{partition['mount_point']}")
     system("mount /dev/#{disk_name}#{partition['number']} /mnt#{partition['mount_point']}")
