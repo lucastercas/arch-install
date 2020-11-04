555d0b0abc0ed6d878731a09b432ba35c05aa786
lucastercas <lucasmtercas@gmail.com>
2020-11-03 22:35:41 -0300

    test(bootloader): Test heredocs to make grub-mkconfig

--- src/chroot/bootloader.rb
+++ src/chroot/bootloader.rb
@@ -3,7 +3,9 @@
 def bootloader(config)
   puts("#--- bootloader ---#")
   system("arch-chroot /mnt mkdir -p /boot/grub")
-  system("arch-chroot /mnt grub-mkconfig > /boot/grub/grub.cfg")
+  system("arch-chroot /mnt /bin/bash <<END 
+  grub-mkconfig > /boot/grub/grub.cfg
+  END")
   system("arch-chroot /mnt grub-install /dev/#{config['disks'][0]['name']}")
   # system("arch-chroot /mnt refind-install")
 end
