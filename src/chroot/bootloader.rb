1c21530fd3ca01af2b34198fa4bcfc351f4ed9ea
lucastercas <lucasmtercas@gmail.com>
2020-11-08 14:39:24 -0300

    fix(bootloader): Change > to -o

--- src/chroot/bootloader.rb
+++ src/chroot/bootloader.rb
@@ -4,7 +4,7 @@ def bootloader(config)
   puts("#--- bootloader ---#")
   system("arch-chroot /mnt mkdir -p /boot/grub")
   system("arch-chroot /mnt /bin/bash <<END 
-  grub-mkconfig > /boot/grub/grub.cfg
+  grub-mkconfig -o /boot/grub/grub.cfg
   END")
   system("arch-chroot /mnt grub-install /dev/#{config['disks'][0]['name']}")
   # system("arch-chroot /mnt refind-install")
